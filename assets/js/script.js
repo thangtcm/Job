5f3afbd09ccc68dc643fec351bd2a11942113ea4
thangtcm <caominhthangtran@gmail.com>
2023-05-07 00:02:48 +0700

    Layout Menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,27 @@ $(function() {
       railVisible: true // hiển thị thanh kéo
     });
   });
+
+
+// Sticky Navbar
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+      $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+  } else {
+      $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+  }
+});
+
+
+// Back to top button
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 300) {
+      $('.back-to-top').fadeIn('slow');
+  } else {
+      $('.back-to-top').fadeOut('slow');
+  }
+});
+$('.back-to-top').click(function () {
+  $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+  return false;
+});
