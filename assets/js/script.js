b73aaf10a2fb3ccaa3262ff79efe968863ab3234
thangtcm <caominhthangtran@gmail.com>
2023-05-11 11:36:55 +0700

    Update Template Job

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,33 @@ $('.back-to-top').click(function () {
   $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
   return false;
 });
+
+
+//Filter
+$(document).on('click', '#filter_search', function() {
+	$('#filter_inputs').slideToggle("slow");
+});
+
+// Checkbox Select
+	
+$('.app-listing .selectbox').on("click", function() {
+	$(this).parent().find('#checkboxes').fadeToggle();
+	$(this).parent().parent().siblings().find('#checkboxes').fadeOut();
+});
+
+//Checkbox Select
+	
+if($('.sortby').length > 0) {
+	var show = true;
+	var checkbox1 = document.getElementById("checkbox");
+	$('.selectboxes').on("click", function() {
+		
+		if (show) {
+			checkbox1.style.display = "block";
+			show = false;
+		} else {
+			checkbox1.style.display = "none";
+			show = true;
+		}
+	});		
+}
